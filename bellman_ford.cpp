68461f8c07b87b66f0172de14a76459adaa133d9
Florian Weber <oss@florianjw.de>
2015-05-11 16:03:42 +0200

    bellman-ford neu geschrieben

--- bellman_ford.cpp
+++ bellman_ford.cpp
@@ -10,41 +10,32 @@
 
 
 using node = std::size_t;
-using edge = std::pair<node, node>;
 using dist = double;
-using graph = std::vector<std::vector<node>>;
-using edge_weights = std::unordered_map<edge, dist>;
 
-namespace std {
-
-template<>
-struct hash<std::pair<node, node>> {
-	std::size_t operator()(const std::pair<node, node>& edge) const {
-		// this is ok, because we know that first != second
-		return edge.first xor edge.second;
-	}
+using edge = struct {
+	node from;
+	node to;
+	dist weight;
 };
-}
 
-std::vector<dist> bellman_ford(const graph& g, const edge_weights& edges, node source) {
-	auto min_dists = std::vector<dist>(g.size(), std::numeric_limits<dist>::infinity());
+const auto inf_dist = std::numeric_limits<dist>::infinity();
+
+std::vector<dist> bellman_ford(std::size_t node_count, const std::vector<edge>& edges, node source) {
+	auto min_dists = std::vector<dist>(node_count, inf_dist);
 	min_dists[source] = 0;
 	auto changes_happened = false;
-	for (auto i = std::size_t{}; i < g.size() + 1; ++i) {
-		for (auto v = std::size_t{}; v < g.size(); ++v) {
-			for (auto neighbour: g[v]) {
-				const auto old_dist = min_dists[neighbour];
-				const auto new_dist =
-					min_dists[v] + edges.at({v, neighbour});
-				if (new_dist < old_dist) {
-					min_dists[neighbour] = new_dist;
-					changes_happened = true;
-				}
+	for (auto i = std::size_t{}; i < node_count + 1; ++i) {
+		for(const auto& e: edges) {
+			const auto old_dist = min_dists[e.to];
+			const auto new_dist = min_dists[e.from] + e.weight;
+			if (new_dist < old_dist) {
+				min_dists[e.to] = new_dist;
+				changes_happened = true;
 			}
 		}
 		if (!changes_happened) {
 			break;
-		} else if(i == g.size()) {
+		} else if(i == node_count) {
 			throw std::runtime_error{"negative cycle"};
 		}
 		changes_happened = false;
@@ -53,21 +44,14 @@ std::vector<dist> bellman_ford(const graph& g, const edge_weights& edges, node s
 }
 
 int main() try {
-	const auto g = graph{
-		{1, 4},
-		{0, 3},
-		{4},
-		{0, 2},
-		{1}
+	const auto edges = std::vector<edge>{
+		{0, 1,  7}, {0, 4,  -1},
+		{1, 0, 10}, {1, 3,  -4},
+		{2, 4,  1},
+		{3, 0,  0}, {3, 2, 2.5},
+		{4, 1, 23}
 	};
-	const auto edges = edge_weights {
-		{{0, 1}, 7}, {{0, 4}, -1},
-		{{1, 0}, 10}, {{1, 3}, -4},
-		{{2, 4}, 1},
-		{{3, 0}, 0}, {{3, 2}, 2.5},
-		{{4, 1}, 23}
-	};
-	const auto min_dists = bellman_ford(g, edges, 0);
+	const auto min_dists = bellman_ford(5, edges, 0);
 	std::copy(min_dists.begin(), min_dists.end(), std::ostream_iterator<dist>{std::cout, "\n"});
 } catch (std::runtime_error& e) {
 	std::cerr << "Error: " << e.what() << '\n';
