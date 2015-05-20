0c8cc499f1833279a6807a5f98c713e302ef9a2d
moritzuehling <github@moritzuehling.de>
2015-05-20 09:13:35 +0200

    Finally committed code for dijskstra

---
+++ dijkstra.cpp
@@ -0,0 +1,80 @@
+#include<vector>
+#include<algorithm>
+#include<queue> // not priority_queue!
+#include<iostream>
+
+using namespace std;
+
+struct edge {
+	int to;
+	double weight;
+};
+
+
+bool operator < (const edge& e1, const edge& e2) {
+    return e1.weight < e2.weight;
+}
+
+
+using node = vector<edge>;
+
+vector<double> dijkstra(vector<node>& nodes, int startnode) {
+	// initialize all distances with infinity
+	vector<double> distances (nodes.size(), 100000000000);
+
+	priority_queue<edge> todo;
+	
+	edge start;
+	start.to = startnode;
+	start.weight = 0;
+
+	todo.push(start);
+
+	while(todo.size() > 0) {
+		auto current = todo.top();
+		todo.pop();
+
+		// only if there is no better way to reach the node
+		if(current.weight < distances[current.to]) {
+			distances[current.to] = current.weight;
+
+			for(int i = 0; i < nodes[current.to].size(); i++) {
+				edge next = nodes[current.to][i];
+				next.weight += current.weight;
+
+				todo.push(next);
+			}
+		}
+	}
+
+	return distances;
+}
+
+
+int main() {
+	vector<node> nodes = { 
+		{ 
+			// start
+			{ 1, 1 },
+			{ 2, 3 }
+		},
+		{ 
+			{ 3, 3 },
+		},
+		{ 
+			{ 4, 4 },
+		},
+		{
+			{ 4, 1 }
+		},
+		{
+			// ziel
+		}		
+	};
+
+	auto res = dijkstra(nodes, 0);
+
+	for(int i = 0; i < res.size(); i++) {
+		cout << "res[" << i << "] = " << res[i] << endl;
+	}
+}
